58cde25dd28016f502b77b7d1d2f13c31402a5d9
unknown <selvapriya2005@gmail.com>
2024-05-07 23:53:02 +0530

    Initial commit

---
+++ src/main/java/com/edureka/mavenjenkinsdemo/MavenJenkinsDemoApplication.java
@@ -0,0 +1,18 @@
+package com.edureka.mavenjenkinsdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@SpringBootApplication
+public class MavenJenkinsDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MavenJenkinsDemoApplication.class, args);
+	}
+	@GetMapping("/")
+    public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+      return String.format("Hello %s!", name);
+	}
+}
---
+++ src/test/java/com/edureka/mavenjenkinsdemo/MavenJenkinsDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edureka.mavenjenkinsdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MavenJenkinsDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
